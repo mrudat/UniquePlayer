6b2e2b901caf124ea8c04ef17f5e75d404e2ecda
Martin Rudat <mrudat@toraboka.com>
2021-03-21 14:01:28 +1100

    more tests

--- Tests/Tests.cs
+++ Tests/Tests.cs
@@ -3,12 +3,14 @@ using Xunit;
 using UniquePlayer;
 using System.IO.Abstractions.TestingHelpers;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Tests
 {
     public class Tests
     {
         public static readonly string TexturePath = @"Textures\";
+        public static readonly string MeshesPath = @"Meshes\";
 
         public static readonly TheoryData<string?, string?, bool> ChangeTexturePathData = new()
         {
@@ -29,6 +31,57 @@ namespace Tests
             var newPath = program.ChangeTexturePath(oldPath, ref changed, TexturePath);
             Assert.Equal(expectedPath, newPath);
             Assert.Equal(expectedChanged, changed);
+
+            if (oldPath == null) return;
+            if (!expectedChanged) return;
+
+            Assert.True(program.replacementTexturePathDict.TryGetValue(oldPath, out var replacementPath));
+            Assert.Equal(expectedPath, replacementPath);
+        }
+
+
+        public class MangleMeshesPathData : TheoryData<string, string, string, string> {
+            public MangleMeshesPathData() {
+                foreach (var injectedPath in new[] { "Player", @"Player\Meshes", "Other" })
+                    foreach (var originalPath in new[] { @"Meshes\mesh.nif", "mesh.nif" })
+                        Add(originalPath, injectedPath, $@"Meshes\{injectedPath}\mesh.nif", $@"{injectedPath}\mesh.nif");
+            }
+        }
+
+        [Theory]
+        [ClassData(typeof(MangleMeshesPathData))]
+        public void TestMangleMeshesPath(string originalPath, string injectedPath, string expectedPath, string expectedTestPath)
+        {
+            var newPath = Program.MangleMeshesPath(originalPath, injectedPath, out var testPath);
+            Assert.Equal(expectedPath, newPath);
+            Assert.Equal(expectedTestPath, testPath);
+        }
+
+
+        public static readonly TheoryData<string?, string?, bool> ChangeMeshPathData = new()
+        {
+            { "original.nif", "original.nif", false },
+            { "replaced.nif", @"Meshes\Player\replaced.nif", true },
+            { @"Meshes\replaced.nif", @"Meshes\Player\replaced.nif", true }
+        };
+
+        [Theory]
+        [MemberData(nameof(ChangeMeshPathData))]
+        public void TestChangeMeshPath(string oldPath, string expectedPath, bool expectedChanged)
+        {
+            Program program = new(new MockFileSystem(new Dictionary<string, MockFileData>{
+                { @"Meshes\Player\replaced.nif", new("") },
+            }));
+
+            bool changed = false;
+            var newPath = program.ChangeMeshPath(oldPath, ref changed, MeshesPath);
+            Assert.Equal(expectedPath, newPath);
+            Assert.Equal(expectedChanged, changed);
+
+            if (!expectedChanged) return;
+
+            Assert.True(program.replacementMeshPathDict.TryGetValue(oldPath, out var replacementPath));
+            Assert.Equal(expectedPath, replacementPath);
         }
 
     }
