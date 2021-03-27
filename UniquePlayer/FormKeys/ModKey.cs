8c0302bec55f73097ffdd3f9a6263622e3aba8b4
Martin Rudat <mrudat@toraboka.com>
2021-03-27 23:56:31 +1100

    refactor: mark generated code as generated.

--- UniquePlayer/FormKeys/ModKey.cs
+++ UniquePlayer/FormKeys/ModKey.cs
@@ -1,5 +1,8 @@
+using System.CodeDom.Compiler;
+
 namespace Mutagen.Bethesda.FormKeys.SkyrimSE
 {
+    [GeneratedCode("Mutagen.Bethesda.FormKeys.Generator", "2.0.0")]
     public static partial class RaceCompatibility
     {
         public readonly static ModKey ModKey = ModKey.FromNameAndExtension("RaceCompatibility.esm");
